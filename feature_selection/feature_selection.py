60cb62c7155e48c51774cd65872b872d686cbfb8
Richard-mf <136820669@163.com>
2016-01-14 11:30:42 +0800

    add code ...

--- feature_selection/feature_selection.py
+++ feature_selection/feature_selection.py
@@ -2,4 +2,34 @@
 #-*-coding: utf-8 -*-
 from sklearn.datasets import load_boston
 from sklearn.linear_model import (LinearRegression, Ridge, Lasso, RandomizedLasso)
+from sklearn.feature_selection import RFE, f_regression
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.ensemble import RandomForestRegressor
+import numpy as np
+from minepy import MINE
+
+np.random.seed(0)
+
+size = 750
+X = np.random.uniform(0, 1, (size,14))
+
+#"Friedamn #1" regression problem
+Y = (10 * np.sin(np.pi*X[:,0]*X[:,1]) + 20*(X[:,2] - .5)**2 +
+    10*X[:,3] + 5*X[:,4] + np.random.normal(0,1))
+#Add 3 additional correlated variables (correlated with x1-x3)
+X[:,10:] = X[:,:4] + np.random.normal(0, 0.025, (size,4))
+
+names = ["x%s" % i for i in range(1,15)]
+
+ranks = {}
+
+def rank_to_dict(ranks, names, order=1):
+    minmax = MinMaxScaler()
+    ranks = minmax.fit_transform(order*np.array([ranks]).T).T[0]
+    ranks = map(lambda x: round(x, 2),ranks)
+    return dict(zip(names, ranks ))
+
+lr = LinearRegression(normalize=True)
+lr.fit(X, Y)
+
 
