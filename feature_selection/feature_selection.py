e9052faf8a25722a3d27c57e25191ea575a9b7f5
Richard-mf <136820669@163.com>
2016-01-14 16:35:21 +0800

    finished

--- feature_selection/feature_selection.py
+++ feature_selection/feature_selection.py
@@ -29,7 +29,68 @@ def rank_to_dict(ranks, names, order=1):
     ranks = map(lambda x: round(x, 2),ranks)
     return dict(zip(names, ranks ))
 
+# LinearRegression
 lr = LinearRegression(normalize=True)
 lr.fit(X, Y)
 ranks["Linear reg"] = rank_to_dict(np.abs(lr.coef_), names)
+
+# Ridge
+ridge = Ridge(alpha=7)
+ridge.fit(X, Y)
+ranks["Ridge"] = rank_to_dict(np.abs(ridge.coef_), names)
+
+#Lasso
+lasso = Lasso(alpha=.05)
+lasso.fit(X, Y)
+ranks["Lasso"] = rank_to_dict(np.abs(lasso.coef_), names)
+
+#RandomizedLasso(alpha=0.04)
+rlasso = RandomizedLasso(alpha=0.04)
+rlasso.fit(X, Y)
+ranks["Stability"] = rank_to_dict(np.abs(rlasso.scores_), names)
+
+#stop the search when 5 features are left (they will get equal scores)
+rfe = RFE(lr, n_features_to_select=5)
+rfe.fit(X,Y)
+ranks["RFE"] = rank_to_dict(map(float, rfe.ranking_), names, order=-1)
+
+#RandomForestRegressor
+rf = RandomForestRegressor()
+rf.fit(X,Y)
+ranks["RF"] = rank_to_dict(rf.feature_importances_, names)
+
+#f_regression
+f, pval  = f_regression(X, Y, center=True)
+ranks["Corr."] = rank_to_dict(f, names)
+
+#MINE
+mine = MINE()
+mic_scores = []
+for i in range(X.shape[1]):
+    mine.compute_score(X[:,i], Y)
+    m = mine.mic()
+    mic_scores.append(m)
+
+ranks["MIC"] = rank_to_dict(mic_scores, names)
+
+#-----------
+r = {}
+for name in names:
+    r[name] = round(np.mean([ranks[method][name] 
+                             for method in ranks.keys()]), 2)
+
+methods = sorted(ranks.keys())
+ranks["Mean"] = r
+methods.append("Mean")
+
+print "\t%s" % "\t".join(methods)
+for name in names:
+    print "%s\t%s" % (name, "\t".join(map(str, 
+                         [ranks[method][name] for method in methods])))
+
+
+
+
+
+
 
